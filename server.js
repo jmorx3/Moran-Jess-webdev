33da291c13c27fc53705d937b915750a6a252023
Jess Moran <jessica.moran@gmail.com>
2017-04-21 19:29:25 -0400

    fixed connection

--- server.js
+++ server.js
@@ -14,16 +14,3 @@ require("./assignment/app.js")(app);
 var port = process.env.PORT || 3000;
 
 app.listen(port);
-
-var mongoose = require('mongoose');
-var connectionString = 'mongodb://127.0.0.1:27017/skd_webdev';
-
-if (process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
-}
-
-mongoose.connect(connectionString);
